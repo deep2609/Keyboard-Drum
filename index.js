e59ec877c05487aea114acd31a57b344e81d4f5b
Souradip Mandal <67626543+deep2609@users.noreply.github.com>
2021-03-08 18:04:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-document.query
+
+// To produce sound on mouse click
+var numOfButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numOfButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var key = this.innerHTML;
+    sound(key);
+  });
+}
+// To produce sound on key press
+document.addEventListener("keypress",function(event){
+   var k=event.key;
+   sound(k);
+ });
+
+
+// Function to produce sound
+function sound(key){
+  if (key == 'w') {
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+  }
+  if (key == 'a') {
+    var audio = new Audio("sounds/tom-2.mp3");
+    audio.play();
+  }
+  if (key == 's') {
+    var audio = new Audio("sounds/tom-3.mp3");
+    audio.play();
+  }
+  if (key == 'd') {
+    var audio = new Audio("sounds/tom-4.mp3");
+    audio.play();
+  }
+  if (key == 'j') {
+    var audio = new Audio("sounds/crash.mp3");
+    audio.play();
+  }
+  if (key == 'k') {
+    var audio = new Audio("sounds/kick-bass.mp3");
+    audio.play();
+  }
+  if (key == 'l') {
+    var audio = new Audio("sounds/snare.mp3");
+    audio.play();
+  }
+}
+
